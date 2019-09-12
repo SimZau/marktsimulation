3c4ab83dec884cac7cecba18a2cb55c0df065ee9
simon <simon.zaugg@hispeed.ch>
2019-09-12 18:44:35 +0200

    userform implemented

--- public/js/app.js
+++ public/js/app.js
@@ -1,47 +1,63 @@
 var config = {
-  apiKey: "AIzaSyA2qPtA81cSWRj-tOEwF1fgTmnbweFOnbk",
-  authDomain: "markt-simulation.firebaseapp.com",
-  databaseURL: "https://markt-simulation.firebaseio.com",
-  projectId: "markt-simulation",
-  storageBucket: "",
-  messagingSenderId: "361847436208",
-  appId: "1:361847436208:web:9081c036f1e94c3ae11c75"
+    apiKey: "AIzaSyA2qPtA81cSWRj-tOEwF1fgTmnbweFOnbk",
+    authDomain: "markt-simulation.firebaseapp.com",
+    databaseURL: "https://markt-simulation.firebaseio.com",
+    projectId: "markt-simulation",
+    storageBucket: "",
+    messagingSenderId: "361847436208",
+    appId: "1:361847436208:web:9081c036f1e94c3ae11c75"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
-var db = firebase.firestore();
-const userTom = db.doc("users/Tom");
+let db = firebase.firestore();
+const users = db.collection("users");
 
 const titleHeader = document.querySelector("#title");
 const userForm = document.querySelector("#userForm");
+const simulationDiv = document.querySelector("#simulation");
+
+//todo remove
+localStorage.removeItem('username');
+
+let username = localStorage.getItem('username');
+if (!username) {
+    userForm.style.display = "inline";
+} else {
+    simulationDiv.style.display = "inline";
+}
 
 function save(form) {
-  const name = form.name.value;
-  const age = form.age.value;
-  console.log("Values to save: " + name + ", " + age);
-  userTom.set({
-    name: name,
-    age: age
-  }).then(function() {
-    console.log("Saved!");
-  }).catch(function (error) {
-    console.log("Error: ", error);
-  });
+    userForm.style.display = "none";
+    simulationDiv.style.display = "inline";
+    username = form.name.value;
+    localStorage.setItem('username', username);
+
+    console.log("User to save: " + username);
+    users.doc(username).set({
+        name: username,
+        age: form.age.value,
+    }).then(function () {
+        console.log("Saved!");
+    }).catch(function (error) {
+        console.log("Error: ", error);
+    });
+
+    simulationDiv.style.display = "inline";
 }
 
 getRealtimeUpdates = function () {
-  userTom.onSnapshot(function (tom) {
-    if (tom && tom.exists) {
-      console.log("My name is " + tom.data().name);
-      titleHeader.innerText = "My name is " + tom.data().name;
+    if (username) {
+        users.doc(username).onSnapshot(function (tom) {
+            if (tom && tom.exists) {
+                console.log("My name is " + tom.data().name);
+                titleHeader.innerText = "My name is " + tom.data().name;
+            }
+        });
     }
-  });
 };
 
 getRealtimeUpdates();
 
-
-// var username = localStorage.getItem('username');
 // var user;
 // if (username) {
 //   db.collection("users").where("name", "==", username).get()
